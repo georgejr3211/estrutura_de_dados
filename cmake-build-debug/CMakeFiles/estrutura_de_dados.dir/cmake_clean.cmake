9087116ed8129b97fa7006bed80b7d968b60f7ad
DESKTOP\George <georgefeitosajr12@gmail.com>
2018-04-12 23:35:15 -0300

    Commit da estrutura inicial

---
+++ cmake-build-debug/CMakeFiles/estrutura_de_dados.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/estrutura_de_dados.dir/main.c.obj"
+  "estrutura_de_dados.pdb"
+  "estrutura_de_dados.exe"
+  "estrutura_de_dados.exe.manifest"
+  "libestrutura_de_dados.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/estrutura_de_dados.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
